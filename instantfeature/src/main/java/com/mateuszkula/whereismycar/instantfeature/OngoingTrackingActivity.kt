fcb89fe452f2ac3f4f67481bd1e9da6b99fa0bf1
Mateusz Kula <mateusz.kula@jet.com>
2018-02-22 21:33:22 +0000

    Showing tracking lat and lon

--- instantfeature/src/main/java/com/mateuszkula/whereismycar/instantfeature/OngoingTrackingActivity.kt
+++ instantfeature/src/main/java/com/mateuszkula/whereismycar/instantfeature/OngoingTrackingActivity.kt
@@ -2,6 +2,8 @@ package com.mateuszkula.whereismycar.instantfeature
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.content.SharedPreferences
+import kotlinx.android.synthetic.main.activity_ongoingtracking.*
 
 /**
  * Created by mateusz.kula on 1/24/18.
@@ -9,10 +11,22 @@ import android.support.v7.app.AppCompatActivity
 
 class OngoingTrackingActivity : AppCompatActivity() {
 
+    private val PREFS_FILENAME = "abc"
+
+    var sharedPreferences : SharedPreferences? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_ongoingtracking)
+
+        sharedPreferences = getSharedPreferences(PREFS_FILENAME, 0)
+
+        var longitude = sharedPreferences!!.getFloat("longitude", 0f)
+        var latitude = sharedPreferences!!.getFloat("latitude", 0f)
+
+        latitudeView.setText(latitude.toString())
+        longitudeView.setText(longitude.toString())
     }
 
 }
